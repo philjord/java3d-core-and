3917d7a1e6b490b79ddeb88416336f25846d74b5
phil <philjord@ihug.co.nz>
2016-04-02 18:13:39 +0200

    DANGER! j3dMessage creates reduced, but touches some core gear, be suspicious!

--- src/javax/media/j3d/TransformGroupRetained.java
+++ src/javax/media/j3d/TransformGroupRetained.java
@@ -122,36 +122,48 @@ ArrayList<NodeRetained> childTransformLinks = new ArrayList<NodeRetained>(1);
    * the passed transform.
    * @param t1 the transform to be copied
    */
+	// re-use previous to avoid object creation, if nothing is still using it
+	private J3dMessage prevMessage;
+	private Transform3D trans = new Transform3D();
   void setTransform(Transform3D t1) {
-      J3dMessage tchangeMessage = null;
-      int i, j;
-      Transform3D trans = null;
+       
+      //int i, j;
+	  J3dMessage tchangeMessage = null;
+	  if (prevMessage != null && prevMessage.getRefcount() == 0)
+	  {
+		  tchangeMessage = prevMessage;
+	  }
+	  else
+	  {
+		  tchangeMessage = new J3dMessage();
+		  // trans will still be in use as well in prev message re-create
+		  trans = new Transform3D();
+	  }
 
       if (staticTransform != null) {
 	  // this writeable transformGroup has a static transform
 	  // merged into this node
 
-      	  trans = new Transform3D(staticTransform.transform);
-	  trans.mul(t1);
+      	  trans.set(staticTransform.transform);
+      	  trans.mul(t1);
 
       	  transform.setWithLock(trans);
 
       } else {
-      	  trans = new Transform3D(t1);
+      	  trans.set(t1);
       	  transform.setWithLock(t1);
       }
 
       if (transformChange != null) {
-	  notifyConditions();
+    	  notifyConditions();
       }
 
       if (source.isLive()) {
 
 	  if (aboveAViewPlatform && !t1.isCongruent()) {
 	      throw new BadTransformException(J3dI18N.getString("ViewPlatformRetained0"));
-	  }
-
-	  tchangeMessage = new J3dMessage();
+	  }	  
+	  
 	  tchangeMessage.type = J3dMessage.TRANSFORM_CHANGED;
 	  tchangeMessage.threads = targetThreads;
 	  tchangeMessage.args[1] = this;
@@ -160,6 +172,7 @@ ArrayList<NodeRetained> childTransformLinks = new ArrayList<NodeRetained>(1);
 	  tchangeMessage.universe = universe;
 	  //System.err.println("TransformGroupRetained --- TRANSFORM_CHANGED " + this);
 	  VirtualUniverse.mc.processMessage(tchangeMessage);
+	  prevMessage = tchangeMessage;
       }
       dirtyBoundsCache();
   }
--- src/javax/media/j3d/WakeupCondition.java
+++ src/javax/media/j3d/WakeupCondition.java
@@ -36,111 +36,136 @@ import java.util.Enumeration;
  * scheduler hands back an enumeration of that WakeupCondition.
  */
 
-public abstract class WakeupCondition extends Object {
+public abstract class WakeupCondition extends Object
+{
 
-    static final int ALL_ELEMENTS = 0;
-    static final int TRIGGERED_ELEMENTS = 1;
+	static final int ALL_ELEMENTS = 0;
+	static final int TRIGGERED_ELEMENTS = 1;
 
-    /**
-     * This boolean indicates whether this condition has been fully met.
-     */
-    boolean conditionMet = false;
+	/**
+	 * This boolean indicates whether this condition has been fully met.
+	 */
+	boolean conditionMet = false;
 
-    /**
-     * This points to the parent of this criterion in the AndOr tree
-     */
-    WakeupCondition parent = null;
+	/**
+	 * This points to the parent of this criterion in the AndOr tree
+	 */
+	WakeupCondition parent = null;
 
-    /**
-     * The location of this criterion in the parents array.
-     */
-    int id;
+	/**
+	 * The location of this criterion in the parents array.
+	 */
+	int id;
 
-    /**
-     * The BehaviorRetained node that is using this condition
-     */
-    BehaviorRetained behav = null;
+	/**
+	 * The BehaviorRetained node that is using this condition
+	 */
+	BehaviorRetained behav = null;
 
-    /**
-     * This is the allElements enumerator
-     */
-    WakeupCriteriaEnumerator allEnum = null;
+	/**
+	 * This is the allElements enumerator
+	 */
+	WakeupCriteriaEnumerator allEnum = null;
 
-    /**
-     * This is the triggeredElements enumerator
-     */
-    WakeupCriteriaEnumerator trigEnum = null;
+	/**
+	 * This is the triggeredElements enumerator
+	 */
+	WakeupCriteriaEnumerator trigEnum = null;
 
-    // Use in WakeupIndexedList
-    int listIdx[][];
+	// Use in WakeupIndexedList
+	int listIdx[][];
 
-    /**
-     * Returns an enumeration of all WakeupCriterias in this Condition.
-     */
-    public Enumeration allElements() {
-	if (allEnum == null) {
-	    allEnum = new WakeupCriteriaEnumerator(this, ALL_ELEMENTS);
-	} else {
-	    allEnum.reset(this, ALL_ELEMENTS);
+	/**
+	 * Returns an enumeration of all WakeupCriterias in this Condition.
+	 */
+	public Enumeration allElements()
+	{
+		if (allEnum == null)
+		{
+			allEnum = new WakeupCriteriaEnumerator(this, ALL_ELEMENTS);
+		}
+		else
+		{
+			allEnum.reset(this, ALL_ELEMENTS);
+		}
+		return allEnum;
 	}
-	return allEnum;
-    }
 
-    /**
-     * Returns an enumeration of all triggered WakeupCriterias in this Condition.
-     */
-    public Enumeration triggeredElements() {
-	if (trigEnum == null) {
-	    trigEnum = new WakeupCriteriaEnumerator(this, TRIGGERED_ELEMENTS);
-	} else {
-	    trigEnum.reset(this, TRIGGERED_ELEMENTS);
+	/**
+	 * Returns an enumeration of all triggered WakeupCriterias in this Condition.
+	 */
+	public Enumeration triggeredElements()
+	{
+		if (trigEnum == null)
+		{
+			trigEnum = new WakeupCriteriaEnumerator(this, TRIGGERED_ELEMENTS);
+		}
+		else
+		{
+			trigEnum.reset(this, TRIGGERED_ELEMENTS);
+		}
+		return trigEnum;
 	}
-	return trigEnum;
-    }
 
-    /**
-     * this sets the conditionMet flag.
-     */
-    void setConditionMet(int id, Boolean checkSchedulingRegion) {
-
-	if (!conditionMet) {
-	    conditionMet = true;
-	    J3dMessage message = new J3dMessage();
-	    message.type = J3dMessage.COND_MET;
-	    message.threads = J3dThread.UPDATE_BEHAVIOR;
-	    message.universe = behav.universe;
-	    message.args[0] = behav;
-	    message.args[1] = checkSchedulingRegion;
-	    message.args[2] = this;
-	    VirtualUniverse.mc.processMessage(message);
+	/**
+	 * this sets the conditionMet flag.
+	 */
+	// re-use previous to avoid object creation, if nothing is stillusing it
+	private J3dMessage prevMessage;
+	void setConditionMet(int id, Boolean checkSchedulingRegion)
+	{
+		if (!conditionMet)
+		{
+			conditionMet = true;
+			J3dMessage message = null;
+			if (prevMessage != null && prevMessage.getRefcount() == 0)
+			{
+				message = prevMessage;
+			}
+			else
+			{
+				message = new J3dMessage();
+			}
+			message.type = J3dMessage.COND_MET;
+			message.threads = J3dThread.UPDATE_BEHAVIOR;
+			message.universe = behav.universe;
+			message.args[0] = behav;
+			message.args[1] = checkSchedulingRegion;
+			message.args[2] = this;
+			VirtualUniverse.mc.processMessage(message);
+			prevMessage = message;
+		}
 	}
-    }
 
-    /**
-     * Initialize And/Or tree and add criterion to the BehaviourStructure
-     */
-    void buildTree(WakeupCondition parent, int id, BehaviorRetained b){
-	this.parent = parent;
-	this.behav = b;
-	this.id = id;
-	conditionMet = false;
-    }
+	/**
+	 * Initialize And/Or tree and add criterion to the BehaviourStructure
+	 */
+	void buildTree(WakeupCondition parent, int id, BehaviorRetained b)
+	{
+		this.parent = parent;
+		this.behav = b;
+		this.id = id;
+		conditionMet = false;
+	}
 
-    /**
-     * This goes through the AndOr tree to remove the various criterion from the
-     * BehaviorStructure.
-     * We can't use  behav.universe.behaviorStructure since behav
-     * may reassign to another universe at this time.
-     */
-    void cleanTree(BehaviorStructure bs) {
-	conditionMet = false;
-    }
+	/**
+	 * This goes through the AndOr tree to remove the various criterion from the
+	 * BehaviorStructure.
+	 * We can't use  behav.universe.behaviorStructure since behav
+	 * may reassign to another universe at this time.
+	 */
+	void cleanTree(BehaviorStructure bs)
+	{
+		conditionMet = false;
+	}
 
-    void reInsertElapseTimeCond() {
-	conditionMet = false;
-    }
+	void reInsertElapseTimeCond()
+	{
+		conditionMet = false;
+	}
 
-    void resetTree() {
-	conditionMet = false;
-    }
+	void resetTree()
+	{
+		conditionMet = false;
+	}
 }
