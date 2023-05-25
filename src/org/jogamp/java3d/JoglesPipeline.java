433027c2cbf7f9061e36c3b2bb2b9b99aaaadfc5
Philip Jordan <p.j.nz@outlook.com>
2023-05-25 12:49:45 +1200

    jogl updated to 2.4.0

--- src/org/jogamp/java3d/JoglesPipeline.java
+++ src/org/jogamp/java3d/JoglesPipeline.java
@@ -47,6 +47,7 @@ import org.jogamp.vecmath.Vector4f;
 
 import com.jogamp.common.nio.Buffers;
 import com.jogamp.common.os.Platform;
+import com.jogamp.common.util.VersionNumber;
 import com.jogamp.nativewindow.AbstractGraphicsDevice;
 import com.jogamp.nativewindow.NativeSurface;
 import com.jogamp.nativewindow.OffscreenLayerOption;
@@ -7381,8 +7382,8 @@ public class JoglesPipeline extends Jogl2es2DEPPipeline
 				}
 
 				if (Platform.getOSName().equalsIgnoreCase("Windows 10") && //win10
-						(Platform.JAVA_VERSION_NUMBER.compareTo(Platform.Version18) > 0) || // 1.9 or 1.8 > 51 //Version 19 was swapped to version 9 due to JEP 223 in jogl 2.4.0
-						(Platform.JAVA_VERSION_NUMBER.compareTo(Platform.Version18) >= 0 && Platform.JAVA_VERSION_UPDATE > 51) && //
+						(Platform.JAVA_VERSION_NUMBER.compareTo(new VersionNumber(1, 8, 0)) > 0) || // 1.9 or 1.8 > 51 //Version 19 was swapped to version 9 due to JEP 223 in jogl 2.4.0
+						(Platform.JAVA_VERSION_NUMBER.compareTo(new VersionNumber(1, 8, 0)) >= 0 && Platform.JAVA_VERSION_UPDATE > 51) && //
 								Platform.getJavaVMName().toLowerCase().startsWith("java hotspot(tm)"))// e.g. Java HotSpot(TM) 64-Bit Server VM ; OpenJDK would give OpenJDK 64-Bit Server VM
 				{
 					System.err.println("Issue: The use of an Intel HD2000/3000 driver in combination with Windows 10 and");
